7f3bb537359b5fd27e55811b3c8dacc5dfad1adb
Juveria <shaikhjuveriya71@gmail.com>
2025-02-18 17:45:38 +0530

    Revert "Changed added"
    This reverts commit a9d9348da1a3529aa282ef9300b19ce77c474638.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import FooterSection from './pages/Home/FooterSection.jsx'; // Adjust the path if necessary  
 
 const App = () => {
   return (
-    <><div>App</div>
-       <FooterSection />
-    </>  
+    <div>App</div>
   )
 }
 
