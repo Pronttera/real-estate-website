a09fb895c2dafd94e9e28df6d304d85d324521aa
kashish shaikh <kashish.shaikh9822@gmail.com>
2025-02-20 18:16:18 +0530

    Fixed the content size and other things in HeroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import HeroSection from './pages/Home/HeroSection.jsx'
-import Section1 from './Section1.jsx'
-import Section2 from './Section2.jsx'
+import Home from './pages/Home/index.jsx'
 
 function App() {
   return(
-    <>
-      <HeroSection/>
-      <Section1/>
-      <Section2/>
-    </>
+   <Home/>
   )
 }
 
