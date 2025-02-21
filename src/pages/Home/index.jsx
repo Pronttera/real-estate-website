1ab5eede34fb55d9456319cd7f5fdc0f044a6d41
Juveria Shaikh <shaikhjuveriya71@gmail.com>
2025-02-21 16:50:23 +0530

    Footer Section Added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import HeroSection from './HeroSection'
-import FooterSection from './pages/Home/FooterSection.jsx';
 
 const Home = () => {
   return (
 
   <><div>App home page</div>
           <HeroSection/>
-         <FooterSection />
+        
   </>       
    
   )
