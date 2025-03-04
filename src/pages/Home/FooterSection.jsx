a489d1546c4a5b5eeeb8825f9680e81156710643
Umar Shaikh <umarsk9921@gmail.com>
2025-03-05 01:16:47 +0530

    changes

--- src/pages/Home/FooterSection.jsx
+++ src/pages/Home/FooterSection.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import mailIcon from '../../assets/images/mail-icon.png';
+import React from "react";
+import mailIcon from "../../assets/images/mail-icon.png";
 import { FaFacebookF } from "react-icons/fa6";
 import { ImLinkedin2 } from "react-icons/im";
 import { FaTwitter } from "react-icons/fa6";
@@ -12,12 +12,17 @@ const Footer = () => {
   return (
     <footer className=" bottom-0 bg-[#222] text-white w-full ">
       <div className="flex flex-col gap-4 p-2  md:flex-row md:p-24 ">
-        <div className="w-full flex flex-col justify-between h-full mr-36  md:w-1/4"> 
-                                                                           {/* 25% */}
-            {/* <img src={Zenith Estates} alt="Zenith Estates Logo" className="mb-5" /> */}
-            <p>Search over 1 million listings including apartments, houses, condos, and townhomes available for rent. You'll find your next home, in any style you prefer.</p>
+        <div className="w-full flex flex-col justify-between h-full mr-36  md:w-1/4">
+          {/* 25% */}
+          <div className="text-3xl md:text-4xl font-extrabold text-gray-900 ">
+          Zenith <span className="text-[#72B944]">Estates</span>
+          </div>
+          <p>
+            Search over 1 million listings including apartments, houses, condos,
+            and townhomes available for rent. You'll find your next home, in any
+            style you prefer.
+          </p>
           <div className="flex items-center justify-center gap-4 text-[#72B944] p-5 my-14">
-      
             <FaFacebookF size={25} />
             <ImLinkedin2 size={24} />
             <FaTwitter size={25} />
@@ -25,58 +30,76 @@ const Footer = () => {
             <FaYoutube size={28} />
           </div>
           <div className="flex flex-col items-start justify-center">
-                                   <h3 className="mb-5 text-lg font-bold text-white">Contact Info :</h3>
+            <h3 className="mb-5 text-lg font-bold text-white">
+              Contact Info :
+            </h3>
             <div className="w-full flex flex-col gap-4">
-                    <div className="w-full flex items-center flex-row gap-4">
-                    <HiPhone  size={28}  />
-                                            <p>(888) 111-2222</p>
-                    </div>
-                    <div className="w-full flex items-center flex-row gap-4">
-                                            <img src={mailIcon} alt="Mail Icon" className="h-8" />
-                                            <p>support@zenithestates.com</p>
-                    </div>
-                    <div className="w-full flex items-center flex-row gap-4">
-                    <IoLocationSharp size={28} />                                            
-                    <p>123 Urban Street, Cityville, USA</p>
-                    </div>
-                    </div>
+              <div className="w-full flex items-center flex-row gap-4">
+                <HiPhone size={28} />
+                <p> 9856425721</p>
+              </div>
+              <div className="w-full flex items-center flex-row gap-4">
+                <img src={mailIcon} alt="Mail Icon" className="h-8" />
+                <p>support@zenithestates.com</p>
+              </div>
+              <div className="w-full flex items-center flex-row gap-4">
+                <IoLocationSharp size={28} />
+                <p>123 Urban Street, Cityville, USA</p>
+              </div>
+            </div>
           </div>
         </div>
 
-        {['ABOUT US', 'ADVERTISERS', 'RENTAL MANAGER', 'THE MARKETPLACE'].map((title, index) => (
-          <div key={index} className="w-full md:w-1/4 my-2">
-                    <h3 className="text-[#72B944]">{title}</h3>
-                    <ul className="mt-8 flex flex-col gap-5 font-medium list-none p-0">
-                                {getLinks(title).map((link, idx) => (
-                                    <li key={idx}>{link}</li>
-                                ))}
-                    </ul>
-          </div>
-        ))}
+        {["ABOUT US", "ADVERTISERS", "RENTAL MANAGER", "THE MARKETPLACE"].map(
+          (title, index) => (
+            <div key={index} className="w-full md:w-1/4 my-2">
+              <h3 className="text-[#72B944]">{title}</h3>
+              <ul className="mt-8 flex flex-col gap-5 font-medium list-none p-0">
+                {getLinks(title).map((link, idx) => (
+                  <li key={idx}>{link}</li>
+                ))}
+              </ul>
+            </div>
+          )
+        )}
       </div>
 
       <div className="text-center py-2">
         <div className="border-t border-gray-400 opacity-50 mb-2 -ml-5"></div>
         <p className="opacity-70 mb-8">© 2024 Nazmulgfx Reserved</p>
-      </div> 
+      </div>
     </footer>
   );
 };
 
 const getLinks = (title) => {
   const linksMap = {
-    'ABOUT US': [
-      'About Us', 'Careers', 'Contact Us', 'Legal Notices', 'Privacy Notice'
+    "ABOUT US": [
+      "About Us",
+      "Careers",
+      "Contact Us",
+      "Legal Notices",
+      "Privacy Notice",
     ],
-    'ADVERTISERS': [
-      'Advertise', 'Add a Property', 'Digital Feeds Program', 'Customer Portal', 'Community Voice'
+    ADVERTISERS: [
+      "Advertise",
+      "Add a Property",
+      "Digital Feeds Program",
+      "Customer Portal",
+      "Community Voice",
     ],
-    'RENTAL MANAGER': [
-      'Rental Manager', 'List Your Property For Rent', 'Screen Applicants', 'Create Rental Leases'
+    "RENTAL MANAGER": [
+      "Rental Manager",
+      "List Your Property For Rent",
+      "Screen Applicants",
+      "Create Rental Leases",
     ],
-    'THE MARKETPLACE': [
-      'Apartamentos.com', 'ApartmentFinder.com', 'ForRent', 'ApartmentHomeLiving.com'
-    ]
+    "THE MARKETPLACE": [
+      "Apartamentos.com",
+      "ApartmentFinder.com",
+      "ForRent",
+      "ApartmentHomeLiving.com",
+    ],
   };
 
   return linksMap[title] || [];
